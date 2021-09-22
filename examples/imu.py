e1b5357400bcd1dba18532648ac9afd4d117100b
MrYsLab <MisterYsLab@gmail.com>
2021-09-22 14:31:26 -0400

    Initial commit

--- examples/imu.py
+++ examples/imu.py
@@ -14,8 +14,6 @@
  along with this library; if not, write to the Free Software
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
- DHT support courtesy of Martyn Wheeler
- Based on the DHTNew library - https://github.com/RobTillaart/DHTNew
 """
 
 import sys
@@ -24,32 +22,32 @@ import time
 from tmx_nano2040_wifi import tmx_nano2040_wifi
 
 
-# Enable the IMU for 5 seconds, then disable it for 5 seconds,
-# enable again for 5 seconds and exit.
+# Enable and monitor the onboard IMU (accelerometer and gyroscope).
 
 def the_callback(data):
-    print(f'IMU_REPORT: AX={data[1]} AY={data[2]} AZ={data[3]} GX={data[4]}, GY='
+    formatted_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data[7]))
+
+    print(f'IMU_REPORT at {formatted_time}: AX={data[1]} AY={data[2]} AZ={data[3]} GX'
+          f'={data[4]}, '
+          f'GY='
           f'{data[5]}, GZ={data[6]}')
 
 
 # Create a Telemetrix instance.
-board = tmx_nano2040_wifi.TmxNano2040Wifi(ip_address='192.168.2.174')
+board = tmx_nano2040_wifi.TmxNano2040Wifi(ip_address='192.168.2.246')
 try:
 
-    # set the pin mode to imu, establish the callback
+    # set the pin mode to imu, establish the callback.
     # data should appear in the console
     board.set_pin_mode_imu(callback=the_callback)
 
-    # let run for 5 seconds
-    time.sleep(5)
-    board.set_pin_mode_imu(callback=the_callback, enable=False)
-    time.sleep(5)
+    while True:
+        try:
+            time.sleep(.1)
+        except KeyboardInterrupt:
+            board.shutdown()
+            sys.exit(0)
 
-    board.set_pin_mode_imu(callback=the_callback)
-    time.sleep(5)
-
-    board.shutdown()
-    sys.exit(0)
 except KeyboardInterrupt:
     board.shutdown()
     sys.exit(0)
