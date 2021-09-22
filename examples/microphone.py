22c3218d749bc07705ee2fe418e2a199297f8d5c
MrYsLab <MisterYsLab@gmail.com>
2021-09-22 14:33:10 -0400

    Initial commit

--- examples/microphone.py
+++ examples/microphone.py
@@ -1,5 +1,5 @@
 """
- Copyright (c) 2020 Alan Yorinks All rights reserved.
+ Copyright (c) 2021 Alan Yorinks All rights reserved.
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
@@ -14,8 +14,7 @@
  along with this library; if not, write to the Free Software
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
- DHT support courtesy of Martyn Wheeler
- Based on the DHTNew library - https://github.com/RobTillaart/DHTNew
+
 """
 
 import sys
@@ -23,16 +22,21 @@ import time
 
 from tmx_nano2040_wifi import tmx_nano2040_wifi
 
+# enable and monitor data from the onboard microphone.
+
 
 def the_callback(data):
-    print(f'MICROPHONE_REPORT: channel type: {data[2]}  Value = {data[1]}')
+    formatted_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data[3]))
+
+    print(f'MICROPHONE_REPORT at {formatted_time}: channel type: {data[2]}  Value ='
+          f' {data[1]}')
 
 
 # Create a Telemetrix instance.
-board = tmx_nano2040_wifi.TmxNano2040Wifi(ip_address='192.168.2.174')
+board = tmx_nano2040_wifi.TmxNano2040Wifi(ip_address='192.168.2.246')
 
 try:
-    # Set the DIGITAL_PIN as an output pin
+    # enable the microphone
     board.set_pin_mode_microphone(callback=the_callback)
 
     # listen for 5 seconds
